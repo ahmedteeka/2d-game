8d45f0818b12bc4579e46c5fc7aac41fb5d3eac1
ahmedteeka <ahmedteeka@gmail.com>
2020-04-02 00:38:16 +0300

    moved the location to left to make the animatied car bettter

--- 2D_Game/Assets/Scripts/CarController.cs
+++ 2D_Game/Assets/Scripts/CarController.cs
@@ -6,27 +6,21 @@ using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
-    public float fuel = 1;
-    public float fuleconsumption = .01f;
-
-    public Rigidbody2D carRiggedBody;
+    public float            fuel = 1;
+    public float            fuleconsumption = .01f;
+    public GameObject       Location;
+    public Rigidbody2D      carRiggedBody;
+    public WheelJoint2D     backTire;
+    public WheelJoint2D     frontTire;
+    public Image            FuelIcon;
+    public float            Carspeed = 80;
+    public float            maxCarspeed = 80;
+    public float            carRotation;
+    public float            carMovement;
+    public float            rotationSpeed = 100;
+    public FixedJoystick    Right_joystick;
+    public Joystick         Floatingjoystick;
     
-    public WheelJoint2D backTire;
-    public WheelJoint2D frontTire;
-    //JointMotor2D backTireMotor;
-    JointMotor2D frontTireMotor;
-
-    public Image  FuelIcon;
-    //public float Speed=200;
-    public float Carspeed = 80;
-    //public float MaxCarspeed = 80;
-    public float movement;
-    public float carRotation;
-    public float carMovement;
-    public float rotationSpeed=100;
-   // public Joystick Hjoystick;
-    public Joystick Vjoystick;
-    public Joystick Floatingjoystick;
 
 
     // Update is called once per frame
@@ -36,12 +30,28 @@ public class CarController : MonoBehaviour
         {
             RestartLevel();
         }
-        Debug.Log(Vjoystick.Vertical);
-       
         //-1 to 1
-        carRotation = Floatingjoystick.Horizontal* rotationSpeed;
-        carMovement = Vjoystick.Vertical* Carspeed;
+        carRotation = Floatingjoystick.Horizontal * rotationSpeed;
+        
+        //carMovement = Right_joystick.Vertical * Carspeed;
+
         FuelIcon.fillAmount = fuel;
+        
+        if (Right_joystick.Vertical > 0)
+        {
+            if(Carspeed<maxCarspeed)
+            Carspeed+=20*Time.deltaTime;
+            carMovement = Right_joystick.Vertical * Carspeed;
+
+        }
+
+
+        if (carMovement > 0)
+        {
+            Carspeed -= 1 * Time.deltaTime;
+        }
+        
+        Location.transform.Translate(Vector3.left * carMovement * Time.deltaTime);
     }
 
     public void RestartLevel()
@@ -51,28 +61,32 @@ public class CarController : MonoBehaviour
 
     private void FixedUpdate()
     {
-         //Un check Use Motor
-        if (carMovement == 0f|| fuel <=0)
-            {
-                backTire.useMotor = false;
-                frontTire.useMotor = false;
-            }
+        //Un check Use Motor
+        if (carMovement == 0f || fuel <= 0)
+        {
+          //  backTire.useMotor = false;
+          //    frontTire.useMotor = false;
+        }
 
-           else
-                {
-            backTire.useMotor = true;
-            frontTire.useMotor = true;
-            JointMotor2D motor = new JointMotor2D { motorSpeed = (carMovement *600 * Time.fixedDeltaTime), maxMotorTorque = 10000 };
-                   backTire.motor = motor;
-                    frontTire.motor = motor;
+        else
+        {
+           // transform.Translate(Vector3.left * Carspeed * Time.deltaTime);
+           // backTire.useMotor = true;
+            // frontTire.useMotor = true;
            
+          //  JointMotor2D motor = new JointMotor2D { motorSpeed = (carMovement * 800 * Time.fixedDeltaTime), maxMotorTorque = 10000 };
+
+        //    backTire.motor = motor;
+          //  frontTire.motor = motor;
         }
         if (fuel > 0)
         {
-            fuel -= fuleconsumption * Mathf.Abs(Vjoystick.Vertical) * Time.fixedDeltaTime;
+          //  fuel -= fuleconsumption * Mathf.Abs(Right_joystick.Vertical) * Time.fixedDeltaTime;
         }
-        // rotate the car   
 
-        carRiggedBody.AddTorque(-carRotation *rotationSpeed * Time.fixedDeltaTime);
+
+        // rotate the car   
+           carRiggedBody.AddTorque(-carRotation * rotationSpeed * Time.fixedDeltaTime);
+        
     }
 }
